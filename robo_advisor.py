eb823cc89150880df961315093024b38b80895c7
ebjacoby <65738833+ebjacoby@users.noreply.github.com>
2020-06-09 23:25:30 -0500

    Get latest refresh date

--- robo_advisor.py
+++ robo_advisor.py
@@ -13,10 +13,31 @@ response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
 
-breakpoint()
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
 
 
+#breakpoint()
+
+
+# app/robo_advisor.py
+
+print("-------------------------")
+print("SELECTED SYMBOL: XYZ")
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: 2018-02-20 02:00pm")
+print("-------------------------")
+print(f"LATEST DAY: {last_refreshed}")
+print("LATEST CLOSE: $100,000.00")
+print("RECENT HIGH: $101,000.00")
+print("RECENT LOW: $99,000.00")
+print("-------------------------")
+print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print("HAPPY INVESTING!")
+print("-------------------------")
 
 
 
