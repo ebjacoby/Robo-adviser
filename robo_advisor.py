dcdbba03e00577ab25c7ae6ad2a7165da0c876b1
ebjacoby <65738833+ebjacoby@users.noreply.github.com>
2020-06-09 23:57:08 -0500

    Update robo_advisor.py

--- robo_advisor.py
+++ robo_advisor.py
@@ -5,8 +5,13 @@
 import requests
 import json # included in python language already
 
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
+
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
+
 response = requests.get(request_url)
+
 # print(type(response)) #> class 'requests.models.Response'
 # print(response.status_code) #> 200 (count)
 # print(response.text) #> str (so need JSON module to process into dictionary)
@@ -15,7 +20,7 @@ parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-
+latest_close = parsed_response["Time Series (Daily)"]["2020-06-09"]["4. close"]
 
 #breakpoint()
 
@@ -26,10 +31,10 @@ print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print("REQUEST AT: 2018-02-20 02:00pm") #DATETIME MODULE
 print("-------------------------")
-print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST DAY: {last_refreshed}")#format string
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
